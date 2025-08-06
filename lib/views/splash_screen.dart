e164082e3f4af57083207e2e614b0fe9658f0bfc
vishag <vvishag477@gmail.com>
2025-08-06 09:24:58 +0530

    added splash_screen

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,30 +1,38 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:noviindus_ayurvedic_patient_app/core/image_const.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  //navigation trigger
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      context.go('/login');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                context.push("/home");
-              },
-              child: Text("tap here for home screen"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                context.push("/login");
-              },
-              child: Text("tap here login screen"),
-            ),
-          ],
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(AppImages.splashScreenBg),
+            fit: BoxFit.cover,
+          ),
         ),
+        child: Center(child: Image.asset(AppImages.splashScreenLogo)),
       ),
     );
   }
--- lib/views/user screens/register_screen.dart
+++ lib/views/user screens/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:noviindus_ayurvedic_patient_app/core/color_const.dart';
 import 'package:noviindus_ayurvedic_patient_app/core/text_constants.dart';
 import 'package:noviindus_ayurvedic_patient_app/core/text_styles.dart';
 import 'package:noviindus_ayurvedic_patient_app/widgets/custom_text_field_widget.dart';
@@ -14,6 +15,7 @@ class RegisterScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             //name
             CustomTextField(
@@ -44,15 +46,48 @@ class RegisterScreen extends StatelessWidget {
               label: AppText.branch,
               hintText: AppText.selectTheBranch,
             ),
+            SizedBox(height: 30),
 
             //treatmentwidget
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: Text(AppText.treatments, style: AppTextStyles.body1),
+            ),
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 25),
+              height: 65,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: AppColors.primaryLight,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                child: Text(
+                  AppText.addTreatments,
+                  style: AppTextStyles.buttonTextPrimary.copyWith(
+                    color: AppColors.textPrimary,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+
             //
             //total amount
             CustomTextField(label: AppText.totalAmount, hintText: ""),
 
             //discount amount
             CustomTextField(label: AppText.discountAmount, hintText: ""),
+
             //payment cash radio button
+            Padding(
+              padding: const EdgeInsets.only(left: 25, top: 10),
+              child: Text(AppText.treatments, style: AppTextStyles.body1),
+            ),
+
+            SizedBox(height: 20),
+
             //advance amount
             CustomTextField(
               label: AppText.advanceAmount,
@@ -63,6 +98,23 @@ class RegisterScreen extends StatelessWidget {
             //save button
           ],
         ),
+      ),
+    );
+  }
+}
+
+//add treatment widget
+class AddTreatmentWidget extends StatelessWidget {
+  const AddTreatmentWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.25,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: AppColors.primary,
       ),
     );
   }
